2b9eebf3e098fe475b70084421fd08938ffe7fbf
RafiKK98 <rafikk1998@gmail.com>
2024-07-31 14:15:59 +0600

    added cards and charts and table

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,6 @@
+import { ReactNode } from "react";
+import DashboardSkeleton from "@/app/ui/skeletons";
+
+export default function DashboardLoader(): ReactNode {
+  return <DashboardSkeleton />;
+}
--- app/dashboard/page.tsx
+++
@@ -1,5 +0,0 @@
-import { ReactNode } from "react";
-
-export default function DashboardPage(): ReactNode {
-  return <div>Dashboard Page</div>;
-}
