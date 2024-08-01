9e1f64bb1ac7ddd13e039d2b81275751c4774a42
RafiKK98 <rafikk1998@gmail.com>
2024-08-01 13:23:52 +0600

    completed chapter 12, added server actions (create, update, delete)

---
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,31 @@
+import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data";
+import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
+import Form from "@/app/ui/invoices/edit-form";
+import { ReactNode } from "react";
+
+export default async function EditPage({
+  params,
+}: {
+  params: { id: string };
+}): Promise<ReactNode> {
+  const id = params.id;
+  const [invoice, customers] = await Promise.all([
+    fetchInvoiceById(id),
+    fetchCustomers(),
+  ]);
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: "Invoices", href: "/dashboard/invoices" },
+          {
+            label: "Edit Invoices",
+            href: `/dashboard/invoices/${id}/edit`,
+            active: true,
+          },
+        ]}
+      />
+      <Form invoice={invoice} customers={customers} />
+    </main>
+  );
+}
